c34af70549bdcc977e68e84c37c67466e762bad5
Itsuki06 <trinhtung0604@gmail.com>
2026-02-10 22:06:15 +0700

    feat: user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,38 @@
+import { AppDataSource } from "../models/DataSource";
+import { User } from "../models/entities/User.entity";
+import { Role } from "../models/entities/Role.entity";
+
+export class UserService {
+  private static userRepository = AppDataSource.getRepository(User);
+  private static roleRepository = AppDataSource.getRepository(Role);
+
+  static async createUser(
+    name: string,
+    email: string,
+    password: string,
+    roleId: number,
+  ) {
+    const role = await this.roleRepository.findOneBy({ id: roleId });
+    if (!role) {
+      throw new Error("Role not found.");
+    }
+
+    const user = new User();
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    user.role = role;
+
+    return await this.userRepository.save(user);
+  }
+
+  static async getAllUsers() {
+    return await this.userRepository.find({
+      relations: ["role"],
+    });
+  }
+
+  static async getAllRoles() {
+    return await this.roleRepository.find();
+  }
+}
