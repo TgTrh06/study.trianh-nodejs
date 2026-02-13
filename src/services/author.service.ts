3f732bbe98e3ed729bdf3a964c8884f9323da83c
Itsuki06 <trinhtung0604@gmail.com>
2026-02-13 22:46:45 +0700

    feat: author service

---
+++ src/services/author.service.ts
@@ -0,0 +1,22 @@
+import { AppDataSource } from "../models/DataSource";
+import { Author } from "../models/entities/Author.entity";
+
+export class AuthorService {
+  private static authorRepository = AppDataSource.getRepository(Author);
+  
+  static async createAuthor(
+    name: string,
+    biography: string,
+  ) {
+    const author = new Author();
+
+    author.name = name;
+    author.biography = biography;
+
+    await this.authorRepository.save(author);
+  }
+
+  static async getAllAuthor() {
+    return await this.authorRepository.find();
+  }
+}
