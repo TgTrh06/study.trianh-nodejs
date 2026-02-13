35a1bde899d54892053b8c155bf2bc4bed0baea4
Itsuki06 <trinhtung0604@gmail.com>
2026-02-13 22:57:30 +0700

    feat: author route

---
+++ src/routers/author.route.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { AppDataSource } from "../models/DataSource";
+import { Author } from "../models/entities/Author.entity";
+import AuthorController from "../controllers/author.controller";
+
+const router = Router();
+
+// FORM CREATE
+router.get("/create", AuthorController.getCreateForm);
+
+// CREATE
+router.post("/create", AuthorController.createAuthor);
+
+// GET ALL
+router.get("/", AuthorController.getAllAuthor);
+
+export default router;
