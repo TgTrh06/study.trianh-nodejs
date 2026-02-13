57a40fd92a0d3d930bfa163a3786a675f8d55e8a
Itsuki06 <trinhtung0604@gmail.com>
2026-02-13 23:15:13 +0700

    feat: book service

---
+++ src/services/book.service.ts
@@ -0,0 +1,37 @@
+import { error } from "node:console";
+import { AppDataSource } from "../models/DataSource";
+import { Author } from "../models/entities/Author.entity";
+import { Book } from "../models/entities/Book.entity";
+
+export class BookService {
+  private static bookRepository = AppDataSource.getRepository(Book);
+  private static authorRepository = AppDataSource.getRepository(Author);
+
+  static async createBook(
+    title: string,
+    publishedYear: number,
+    authorId: any,
+  ) {
+    const author = await this.authorRepository.findOneBy({ id: authorId })
+    if (!author) {
+      throw new Error("Author not found.");
+    }
+
+    const book = new Book();
+    book.title = title;
+    book.publishedYear = publishedYear;
+    book.author = author;
+    
+    await this.bookRepository.save(book);
+  }
+
+  static async getAllBook() {
+    return await this.bookRepository.find({
+      relations: ["author"],
+    });
+  }
+
+  static async getAllAuthor() {
+    return await this.authorRepository.find();
+  }
+}
